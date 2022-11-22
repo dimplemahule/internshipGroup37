fe8d9a945ef10b153cbe8874387f85a29a1a6c9a
mahesh999333 <105484079+mahesh999333@users.noreply.github.com>
2022-11-22 20:46:15 +0530

    revision

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer')
 const route = require('./router/route.js');
-const { default: mongoose } = require('mongoose');
+const  mongoose  = require('mongoose');
 const app = express();
 
 
-app.use(bodyParser.json());
-app.use(multer().any())
-mongoose.connect("mongodb+srv://manjunathkc:krLwKYZrOSmltQIw@cluster0.clnn3jh.mongodb.net/group37Database", {
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://mahesh999333:mahesh999333@cluster0.tecej.mongodb.net/open-to-intern", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3001, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3001))
+app.listen( 3001, function () {
+    console.log('Express app running on port ' + 3001)
 });
--- src/router/route.js
+++ src/router/route.js
@@ -1,11 +1,11 @@
 const express = require ('express')
 const router = express.Router();
 
-const internController = require ('../controller/internController')
+const {createIntern} = require ('../controller/internController')
 const {createColleges, getCollegeDetails} = require('../controller/collegeController')
 
 router.post ('/functionup/colleges', createColleges)
-router.post  ('/functionup/interns', internController.createIntern)
+router.post  ('/functionup/interns', createIntern)
 router.get ('/functionup/collegeDetails', getCollegeDetails)
 
-module.exports = router
+module.exports = router;
